e24a53e6bb553e48398bcd7c08bf16e1c2db7927
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2022-01-08 17:07:25 +1100

    updt: added reaction and remove reaction, modify add reaction method to avoid multiple ids

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -89,7 +89,7 @@ const thoughtsController = {
 
     // To add a new reaction
     addReaction({params, body}, res) {
-        Thought.findOneAndUpdate({_id: params.thoughtId}, {$push: {reactions: body}}, {new: true, runValidators: true})
+        Thought.findOneAndUpdate({_id: params.thoughtId}, {$addtoset: {reactions: body}}, {new: true, runValidators: true})
         .populate({path: 'reactions', select: '-__v'})
         .select('-__v')
         .then(dbThoughtData => {
